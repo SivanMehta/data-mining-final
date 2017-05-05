8db88cf419a261464fbc19a4f2c670a7c60b6333
Sivan Mehta <Sivanmehta2@gmail.com>
2017-05-05 10:30:34 -0400

    replaced NAs when relevant

--- analysis/load-data.R
+++ analysis/load-data.R
@@ -1,4 +1,4 @@
 rm(list = ls())
-train <- read.csv("./data/flights2015.csv")
-test <- read.csv("./data/flights2016_visible.csv")
-submit <- read.csv("./data/flights2016_guess.csv")
+train <- read.csv("./data/flights2015_clean_u.csv")
+test <- read.csv("./data/flights2016_visible_clean_u.csv")
+submit <- read.csv("./data/flights2016_guess_clean_u.csv")
