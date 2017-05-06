f602b1063d323a601910d14b8c4b6ee71c5eec4c
mcstjohn <mstjohn@andrew.cmu.edu>
2017-05-06 15:40:08 -0400

    Tried linear fits, they don't work well

--- analysis/Linear.R
+++ analysis/Linear.R
@@ -7,7 +7,7 @@ linear.models <- function(train, test, fit){
   predsTrain = ifelse(predsTrain > 0.5, 1, 0)
   errTrain_lm = mean(predsTrain != train$DEP_DEL15)
   
-  predslm = predict(fit, newdata = vis)
+  predslm = predict(fit, newdata = vis, type = "response")
   predslm = ifelse(predslm > 0.5, 1, 0)
   errTest_lm = mean(predslm != vis$DEP_DEL15)
   
@@ -18,7 +18,7 @@ linear.models <- function(train, test, fit){
 fit_lm = lm(DEP_DEL15 ~ weather.delay.ratio.ind, data = train)
 errors1 = linear.models(train, vis, fit_lm)
 
-fit_lm2 = lm(DEP_DEL15 ~ weather.delay.ratio.ind + DAY_OF_YEAR + INDEX, data = train)
+fit_lm2 = lm(DEP_DEL15 ~ weather.delay.ratio.ind + DAY_OF_YEAR, data = train)
 errors2 = linear.models(train, vis, fit_lm2)
 
 fit_lm3 = lm(DEP_DEL15 ~ weather.delay.ratio.ind + arr.delay.ratio.ind + 
