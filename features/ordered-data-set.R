e6a9ce534dfca0235feb035c18979dcdfa9242f6
mcstjohn <mstjohn@andrew.cmu.edu>
2017-05-05 23:13:17 -0400

    Returns a dataset ordered chronologically by scheduled departure time (can easily change to actual departure time if needed)
    It now includes the needed helper function

--- features/ordered-data-set.R
+++ features/ordered-data-set.R
@@ -1,8 +1,22 @@
 
-# Returns a dataset ordered chronolically by scheduled departure time (can easily change to actual
-# departure time in needed)
+# Returns the day number given months and days
+# Note: This is for a non-leap year
+getDays <- function(months, days){
+  monthDays <- c(31,28,31,30,31,30,31,31,30,31,30,31)
+  cumdays <- rep(0, length(days))
+  for (i in 1:length(months)){
+    if (months[i] == 1) {cumdays[i] <- days[i]}
+    else{
+      cumdays[i] <- sum(monthDays[1:(months[i]-1)])+days[i]
+    }
+  }
+  return(cumdays)
+}
 
+# Returns a dataset ordered chronologically by scheduled departure time (can easily 
+# change to actual departure time if needed)
 orderDataSet <- function(df){
+  df$DAY_OF_YEAR = getDays(df$MONTH, df$DAY_OF_MONTH)
   day = 1
   data.at.day1 = df[which(df$DAY_OF_YEAR == day),] # data frame of only day 1, unordered
   chrono.by.day1 = data.at.day1[order(data.at.day1$CRS_DEP_TIME),] # data frame of only day in time order
