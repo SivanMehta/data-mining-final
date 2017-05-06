29c8e2e5483ed19243ab2234b44269e813726f59
Sivan Mehta <Sivanmehta2@gmail.com>
2017-05-06 18:14:57 -0400

    import misclassification error into compilation

--- analysis/compile-submission.r
+++ analysis/compile-submission.r
@@ -1,5 +1,6 @@
 source("./analysis/clean-data.R")
 source("./features/add-features.R")
+source("./analysis/Linear.R")
 
 data.2016 <- add.features(data.2016)
 
@@ -11,7 +12,18 @@ guesses.2016 <- data.2016[which(data.2016$is.guess == 1), ]
 predictions <- ifelse(predict(fit.lm, newdata = guesses.2016) > .5, 1, 0)
 
 delay.guesses <- predictions
-performance.guess <- 0
+performance.guess <- y.hat.2[2]
 team.name <- "Naive Dazed and Confused"
 
 save(list=c("delay.guesses","performance.guess","team.name"), file="stat462final.RData")
+
+check_submission_format = function(x){
+  load(x)
+  print(sprintf("Your team name is %s", team.name))
+  print(sprintf("Your submission has %d guesses and is supposed to have 871", length(delay.guesses)))
+  print(sprintf("You guessed %d delayed flights and %d non-delayed flights",sum(delay.guesses==1), sum(delay.guesses==0)))
+  print(sprintf("You expect a misclassification error of %f", performance.guess))  
+}
+
+
+check_submission_format("stat462final.RData")
