52f697a633cf6431e86d36212540424de3047e37
mcstjohn <mstjohn@andrew.cmu.edu>
2017-05-05 23:36:29 -0400

    added a comment

--- features/weather-delay.R
+++ features/weather-delay.R
@@ -1,6 +1,8 @@
 source("./analysis/clean-data.R")
 train <- read.csv("./data/flights2015.csv")
 
+# We may want to change this to within the last hour instead of the whole day
+
 # Works only with ordered data set
 
 # Change NA values to 0
