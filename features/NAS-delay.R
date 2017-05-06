9a07890d71265ca0462d6a596280896fe533c7fb
Sivan Mehta <Sivanmehta2@gmail.com>
2017-05-06 11:28:12 -0400

    Used weather delay functionality to fix nas delay

--- features/NAS-delay.R
+++ features/NAS-delay.R
@@ -4,24 +4,35 @@
 #### We may want to change this to within the last hour, or within the last X many flights
 
 # Change NA values to 0
-ordered$NAS_DELAY[is.na(ordered$NAS_DELAY)] <- 0
-ordered$NAS_DELAY_IND <- ifelse((ordered$NAS_DELAY > 0) & (ordered$DEP_DEL15) > 0, 1, 0)
 
-with.nas.delay <- function(df){
+with.nas.delay <- function(df) {
+  df <- orderDataSet(df)
+  df$NAS_DELAY_IND <- ifelse((df$NAS_DELAY > 0) & (df$DEP_DEL15) > 0, 1, 0)
   delays = rep(-1, nrow(df))
-  days = c(1:max(df$DAY_OF_YEAR))
+  df$DAY_OF_YEAR <- yday(df$FL_DATE)
+  days = c(1:365)
   day = 1
+  numFlights = 0
   numNASDelay = 0
   for (i in 1:nrow(df)){ #for each flight, count the number of delays that day so far
     if (df$DAY_OF_YEAR[i] != day){
       day = day + 1
       numNASDelay = 0
+      numFlights = 0
     }
-    delays[i] = numNASDelay #on this day how many flights have been delayed so far due to NAS?
-    if (df$NAS_DELAY_IND[i] == 1){ #was this flight NAS delayed?
+    if (numFlights == 0) {
+      delays[i] <- 0
+    } else {
+      delays[i] <- numNASDelay / numFlights
+    }
+    
+    # was this flight actually NAS delayed?
+    if (df$NAS_DELAY_IND[i] == 1) {
       numNASDelay = numNASDelay + 1
     }
+    numFlights = numFlights + 1
   }
-  df$NAS.delay.today = delays
+  
+  df$nas.delay.today = delays
   return(df)
 }
--- features/weather-delay.R
+++ features/weather-delay.R
@@ -3,14 +3,6 @@ train <- read.csv("./data/flights2015.csv")
 
 library(lubridate)
 
-# We may want to change this to within the last hour instead of the whole day
-
-# Works only with ordered data set
-
-# Change NA values to 0
-ordered$WEATHER_DELAY[is.na(ordered$WEATHER_DELAY)] <- 0
-ordered$WEATHER_DELAY_IND <- ifelse((ordered$WEATHER_DELAY > 0) & (ordered$DEP_DEL15 > 0), 1, 0)
-
 with.weather.delay <- function(df){
   df <- orderDataSet(df)
   df$WEATHER_DELAY_IND <- ifelse((df$WEATHER_DELAY > 0) & (df$DEP_DEL15 > 0), 1, 0)
