d7c0bd594146581ba9462c940795fd7a0ad96a1b
Sivan Mehta <Sivanmehta2@gmail.com>
2017-05-09 13:57:54 -0400

    rearranged writeup to leave obvious space for rf explanation

--- analysis/model-table.R
+++ analysis/model-table.R
@@ -1,7 +1,3 @@
-source("./features/add-features.R")
-train <- add.features(train)
-vis <- add.features(vis)
-data.2016 <- add.features(data.2016)
 source("./analysis/Linear.R")
 guesses.2016 <- data.2016[which(data.2016$is.guess == 1), ]
 
@@ -9,8 +5,8 @@ guesses.2016 <- data.2016[which(data.2016$is.guess == 1), ]
 # logistic regression, complete linkage and prototype clustering, svms, naive bayes
 # and additive models with training error, test error, and number of delays predicted in guess set
 
-models <- c("base rate", "linear model", "random forest", "complete linkage clustering",
-            "prototype clustering", "svm", "mixture models")
+models <- c("base rate", "mixture models", "linear model", "complete linkage clustering",
+            "prototype clustering", "svm", "random forest")
 model.table <- as.data.frame(models)
 model.table$training.error <- 1
 model.table$test.error <- ""
@@ -24,25 +20,24 @@ to.percent <- function(error) {
 model.table[1, 2] <- to.percent(length(which(train$DEP_DEL15 > 0)) / nrow(train))
 model.table[1, 3] <- to.percent(length(which(vis$DEP_DEL15 > 0)) / nrow(vis))
 
+# Mixture Models (from anna's session)
+model.table[2, 2] <- to.percent(.1202)
+model.table[2, 3] <- to.percent(.0657)
+model.table[2, 4] <- 0
+
 # linear model
-model.table[2, 2] <- to.percent(y.hat.2[1])
-model.table[2, 3] <- to.percent(y.hat.2[2])
+model.table[3, 2] <- to.percent(y.hat.2[1])
+model.table[3, 3] <- to.percent(y.hat.2[2])
 y.hat.lm <- predict(fit.lm.2, newdata = guesses.2016)
 predictions.y.hat <- length(which(y.hat.lm > .5))
-model.table[2, 4] <- predictions.y.hat
+model.table[3, 4] <- predictions.y.hat
 
 rm(y.hat.lm)
 rm(predictions.y.hat)
 
-# random forest
-# (copied from someone else's session because it takes really long to run)
-model.table[3, 2] <- to.percent(0.1699)
-model.table[3, 3] <- to.percent(0.0916)
-model.table[3, 4] <- 14
-
 # clustering
-model.table[4, 2] <- to.percent(1 - misclass.com)
-model.table[5, 2] <- to.percent(1 - misclass.proto)
+model.table[4, 2] <- to.percent(misclass.com)
+model.table[5, 2] <- to.percent(misclass.proto)
 
 # svms (values saved from a previous session)
 # select.names <- c("weather.delay.ratio", "CRS_PIT_TIME")
@@ -60,7 +55,8 @@ model.table[6, 2] <- to.percent(.1646)
 # misclass.test <- length(which(vis$DEP_DEL15 == vis.pred)) / nrow(vis)
 model.table[6, 3] <- to.percent(.0894)
 
-# Mixture Models (from anna's session)
-model.table[7, 2] <- to.percent(.1202)
-model.table[7, 3] <- to.percent(.0657)
-model.table[7, 4] <- 0
+# random forest
+# (copied from someone else's session because it takes really long to run)
+model.table[7, 2] <- to.percent(0.1699)
+model.table[7, 3] <- to.percent(0.0916)
+model.table[7, 4] <- 14
